5edc4f1e6b00e47eb07461fa8e75bf2a2d7c6803
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-12-11 13:48:59 +0200

    feat: implement new API to add new course. we apply some validation on course title, course price using express-validator module

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const {body, validationResult} = require('express-validator');
 
 const app = express();
 const portNum = 8080;
+
+app.use(express.json())
 
 const courses = [
     {
@@ -42,6 +45,30 @@ app.get('/api/courses/:courseId', (req, res) => {
     if(!course) return res.status(404).json({msg: "Course not found."});
 
     res.status(200).json(course);
+})
+
+
+// Create new course
+const newCourseValidator = [
+    body('title')
+        .exists().withMessage('Course title is required.')
+        .isString().withMessage('Course title must be string.')
+        .contains('course').withMessage('Course title must end with (course) keyword.')
+        .isLength({min: 8, max: 30}).withMessage('Course title must be between 8 and 30 characters.'),
+
+    body("price")
+        .exists().withMessage('Course price is required.')
+        .isFloat({min: 0, max: 5000}).withMessage('Course price must be number & between 0 to 5000$.')
+]
+app.post('/api/courses', newCourseValidator, (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+
+    const newCourse = req.body;
+    newCourse.id = courses.length + 1;
+    courses.push(newCourse);
+
+    res.status(201).json({msg: "Course created successfully.", course: newCourse});
 })
 
 app.listen(portNum, () => {
