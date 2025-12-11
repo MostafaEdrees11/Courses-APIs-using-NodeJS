d3cbfc6afea43631f96787a7de7f8316086848ce
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-12-11 12:25:48 +0200

    chore: add array of courses to use it when we implement CRUD Operations. We will integrate with DB but later

--- index.js
+++ index.js
@@ -3,6 +3,32 @@ const express = require('express');
 const app = express();
 const portNum = 8080;
 
+const courses = [
+    {
+        id: 1,
+        title: "HTML5 course",
+        price: 500
+    },
+    {
+        id: 2,
+        title: "JS course",
+        price: 800
+    },
+    {
+        id: 3,
+        title: "Node JS course",
+        price: 1000
+    },
+    {
+        id: 4,
+        title: "React JS course",
+        price: 2000
+    },
+]
+
+// Implement CRUD Operations (Create, Read, Update, Delete)
+
+
 
 app.listen(portNum, () => {
     console.log(`Server connected on portNum: ${portNum}`);
