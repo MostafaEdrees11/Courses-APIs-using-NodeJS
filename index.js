f3ab8423cdc3bf89d3f137820e96de7b4d0ac4e2
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-12-12 23:38:24 +0200

    feat: install mongoose to integrate MongoDB in the project
    - Mongoose is Elegant MongoDB object modeling for Node.js.
    - We will use it to interact with DB that is stored in MongoDB.
    - Connect our application to DB Server successfully.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const portNum = 8080;
+
+const url = 'mongodb+srv://mostafaedrees11:nodejs%2Amongodb%23@learn-mongo-db.hk4scg8.mongodb.net/simple-nodejs-project?appName=learn-mongo-db';
+mongoose.connect(url).then(() => {
+    console.log("Connected to DB Server Successfully.");
+});
 
 const coursesRouter = require('./routes/courses.routes');
 
