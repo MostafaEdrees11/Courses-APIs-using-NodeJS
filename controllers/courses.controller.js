8810cb89fbff96fd48697a2e98b86f3e10786c9e
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-12-13 11:35:30 +0200

    perf: update our response message to follow JSend standard
    - JSend Specifications: https://github.com/omniti-labs/jsend
    - Apply only three status for the response: Success, Fail, Error.
    - Each state has its own structure for the response.
    - We create a utils folder to add on it the utils that we will need through the project.
    - We create a util for httpStatusText to use them in response (success, fail, error).

--- controllers/courses.controller.js
+++ controllers/courses.controller.js
@@ -2,58 +2,114 @@ const {validationResult} = require('express-validator');
 
 const courseModle = require('../models/course.model');
 
+const httpStatusText = require('../utils/httpStatusText');
+
 const getAllCourses = async (req, res) => {
     const courses = await courseModle.find();
-    res.status(200).json(courses);
+    res.status(200).json({
+        status: httpStatusText.SUCCESS,
+        data: { courses }
+    });
 };
 
 const getCourse = async (req, res) => {
     try {
         const course = await courseModle.findById(req.params.courseId);    
-        if(!course) return res.status(404).json({msg: "Course not found."});
+        if(!course) {
+            return res.status(404).json({
+                status: httpStatusText.FAIL,
+                data: { course: null }
+            });
+        }
 
-        res.status(200).json(course);
+        res.status(200).json({
+            status: httpStatusText.SUCCESS,
+            data: { course }
+        });
     } catch(error) {
-        return res.status(404).json({msg: "Invalid ObjectId."});
+        return res.status(400).json({
+            status: httpStatusText.ERROR,
+            message: error.message
+        })
     }
 };
 
 const createCourse = async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            status: httpStatusText.FAIL,
+            data: { errors: errors.array() }
+        });
+    }
 
     const newCourse = new courseModle(req.body);
     await newCourse.save();
 
-    res.status(201).json({msg: "Course created successfully.", course: newCourse});
+    res.status(201).json({
+        status: httpStatusText.SUCCESS,
+        data: {course: newCourse}
+    });
 };
 
 const updateCourse = async (req, res) => {
     const errors = validationResult(req);
-    if(!errors.isEmpty()) return res.status(400).json({errors: errors.array()});
+    if(!errors.isEmpty()) {
+        return res.status(400).json({
+            status: httpStatusText.FAIL,
+            data: { errors: errors.array() }
+        });
+    }
 
-    if(!req.body.title && !req.body.price) return res.status(400).json({msg: "You don't provide any data."});
+    if(!req.body.title && !req.body.price) {
+        return res.status(400).json({
+            status: httpStatusText.FAIL,
+            data: { error: "You don't provide any data." }
+        });
+    }
     
     try {
         let course = await courseModle.findById(req.params.courseId);
-        if(!course) return res.status(404).json({msg: "Course not found."});
+        if(!course) {
+            return res.status(404).json({
+                status: httpStatusText.FAIL,
+                data: { course: null }
+            });
+        }
 
-        await course.updateOne({$set: {...req.body}});
-        res.status(202).json({msg: "Course updated successfully."});
+        const updatedCourse = await course.updateOne({$set: {...req.body}});
+        res.status(202).json({
+            status: httpStatusText.SUCCESS,
+            data: null
+        });
     } catch (error) {
-        return res.status(400).json({msg: "Invalid ObjectId."});
+        return res.status(400).json({
+            status: httpStatusText.ERROR,
+            message: error.message
+        })
     }
 };
 
 const deleteCourse = async (req, res) => {
     try {
         const course = await courseModle.findById(req.params.courseId);
-        if(!course) return res.status(404).json({msg: "Course not found."});
+        if(!course) {
+            return res.status(404).json({
+                status: httpStatusText.FAIL,
+                data: { course: null }
+            });
+        }
 
         await course.deleteOne();
-        res.status(202).json({msg: "Course deleted successfully."});
+        res.status(202).json({
+            status: httpStatusText.SUCCESS,
+            data: null
+        });
     } catch(error) {
-        return res.status(400).json({msg: "Invalid ObjectId."});
+        return res.status(400).json({
+            status: httpStatusText.ERROR,
+            message: error.message
+        })
     }
 };
 
