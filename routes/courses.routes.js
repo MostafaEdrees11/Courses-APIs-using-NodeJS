8dcc836aceaebf717c8f913f65b73175daed7e33
Mostafa Edrees <mostafaedrees018@gmail.com>
2025-12-14 23:46:32 +0200

    perf: apply protected routes to createCourse, updateCourse, deleteCourse

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -4,16 +4,17 @@ const router = express.Router();
 
 const coursesController = require('../controllers/courses.controller');
 const coursesMiddlewareValidations = require('../middlewares/courses.validationSchema');
+const verifyJWTtokenMiddleware = require('../middlewares/verifyJWTtoken.middleware');
 
 // Implement CRUD Operations (Create, Read, Update, Delete)
 router.route('/')
         .get(coursesController.getAllCourses)
-        .post(coursesMiddlewareValidations.createCourseValidation(), coursesController.createCourse)
+        .post(verifyJWTtokenMiddleware, coursesMiddlewareValidations.createCourseValidation(), coursesController.createCourse)
 
 router.route('/:courseId')
         .get(coursesController.getCourse)
-        .patch(coursesMiddlewareValidations.updateCourseValidation(), coursesController.updateCourse)
-        .delete(coursesController.deleteCourse)
+        .patch(verifyJWTtokenMiddleware, coursesMiddlewareValidations.updateCourseValidation(), coursesController.updateCourse)
+        .delete(verifyJWTtokenMiddleware, coursesController.deleteCourse)
 
 
 module.exports = {router};
